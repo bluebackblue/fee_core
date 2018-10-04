4e45c4ddb992678041eae67dead71311aa1e951b
blueback <blueback28@gmail.com>
2018-10-04 21:06:49 +0900

    UniVrm

---
+++ fee/Assets/Fee/Script/UniVrm/Config.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/**
+ * Copyright (c) blueback
+ * Released under the MIT License
+ * https://github.com/bluebackblue/fee/blob/master/LICENSE.txt
+ * http://bbbproject.sakura.ne.jp/wordpress/mitlicense
+ * @brief ＵＮＩＶＲＭ。コンフィグ。
+*/
+
+
+/** NUniVrm
+*/
+namespace NUniVrm
+{
+	/** Config
+	*/
+	public class Config
+	{
+		/** ログ。
+		*/
+		public static bool LOG_ENABLE = false;
+
+		/** アサート。
+		*/
+		public static bool ASSERT_ENABLE = true;
+	}
+}
+
---
+++ fee/Assets/Fee/Script/UniVrm/UniVrm.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/**
+ * Copyright (c) blueback
+ * Released under the MIT License
+ * https://github.com/bluebackblue/fee/blob/master/LICENSE.txt
+ * http://bbbproject.sakura.ne.jp/wordpress/mitlicense
+ * @brief ＵＮＩＶＲＭ。
+*/
+
+
+/** NVrm
+*/
+#if(USE_UNIVRM) || true
+namespace NUniVrm
+{
+	/** UniVrm
+	*/
+	public class UniVrm
+	{
+		/** [シングルトン]s_instance
+		*/
+		private static UniVrm s_instance = null;
+
+		/** [シングルトン]インスタンス。作成。
+		*/
+		public static void CreateInstance()
+		{
+			if(s_instance == null){
+				s_instance = new UniVrm();
+			}
+		}
+
+		/** [シングルトン]インスタンス。チェック。
+		*/
+		public static bool IsCreateInstance()
+		{
+			if(s_instance != null){
+				return true;
+			}
+			return false;
+		}
+
+		/** [シングルトン]インスタンス。取得。
+		*/
+		public static UniVrm GetInstance()
+		{
+			#if(UNITY_EDITOR)
+			if(s_instance == null){
+				Tool.Assert(false);
+			}
+			#endif
+
+			return s_instance;			
+		}
+
+		/** [シングルトン]インスタンス。削除。
+		*/
+		public static void DeleteInstance()
+		{
+			if(s_instance != null){
+				s_instance.Delete();
+				s_instance = null;
+			}
+		}
+
+		/** [シングルトン]constructor
+		*/
+		private UniVrm()
+		{
+		}
+
+		/** [シングルトン]削除。
+		*/
+		private void Delete()
+		{
+		}
+
+		/** 更新。
+		*/
+		public void Main()
+		{
+		}
+	}
+}
+#endif
+
