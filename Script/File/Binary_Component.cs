04dc85f200118655465e38dcea9c43722eb8fff1
backblue <blueback28@gmail.com>
2019-04-27 06:08:34 +0900

    バイナリーコンポーネント

---
+++ Script/File/Binary_Component.cs
@@ -0,0 +1,50 @@
+
+
+/**
+ * Copyright (c) blueback
+ * Released under the MIT License
+ * https://github.com/bluebackblue/fee/blob/master/LICENSE.txt
+ * http://bbbproject.sakura.ne.jp/wordpress/mitlicense
+ * @brief ファイル。バイナリーコンポーネント。
+*/
+
+
+/** Fee.File
+*/
+namespace Fee.File
+{
+	/** Binary_Component
+	*/
+	public class Binary_Component : UnityEngine.MonoBehaviour
+	{
+		/** data
+		*/
+		[UnityEngine.SerializeField,UnityEngine.HideInInspector]
+		public byte[] data;
+
+		/** CreatePrefab
+
+		a_output_path : "Assets/xxx.prefab"
+
+		*/
+		#if(UNITY_EDITOR)
+		public static void CreatePrefab(string a_output_path,byte[] a_binary)
+		{
+			UnityEngine.GameObject t_prefab = new UnityEngine.GameObject();
+			t_prefab.name = "prefab_temp";
+			{
+				//バイナリーコンポーネント追加。
+				Binary_Component t_binary = t_prefab.AddComponent<Binary_Component>();
+				t_binary.data = a_binary;
+
+				//保存。
+				bool t_ret;
+				UnityEditor.PrefabUtility.SaveAsPrefabAsset(t_prefab,a_output_path,out t_ret);
+				UnityEngine.Debug.Assert(t_ret);
+			}
+			UnityEngine.GameObject.DestroyImmediate(t_prefab);
+		}
+		#endif
+	}
+}
+
