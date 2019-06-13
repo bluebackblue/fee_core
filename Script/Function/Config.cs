7b29e54cc19259f355ac2ba707a0785c43fa2e82
backblue <blueback28@gmail.com>
2019-06-13 18:55:06 +0900

    ReThrowException

---
+++ Script/Function/Config.cs
@@ -0,0 +1,48 @@
+
+
+/**
+ * Copyright (c) blueback
+ * Released under the MIT License
+ * https://github.com/bluebackblue/fee/blob/master/LICENSE.txt
+ * http://bbbproject.sakura.ne.jp/wordpress/mitlicense
+ * @brief 関数呼び出し。コンフィグ。
+*/
+
+
+/** Fee.Function
+*/
+namespace Fee.Function
+{
+	/** Config
+	*/
+	public class Config
+	{
+		/** ログ。
+		*/
+		public static bool LOG_ENABLE = false;
+
+		/** ログエラー。
+		*/
+		public static bool LOGERROR_ENABLE = true;
+
+		/** アサート。
+		*/
+		public static bool ASSERT_ENABLE = true;
+
+		/** リスロー。
+		*/
+		public static bool RETHROW_ENABLE = false;
+
+
+
+
+		/** USE_ASYNC
+		*/
+		#if(UNITY_WEBGL)
+		public static bool USE_ASYNC = false;
+		#else
+		public static bool USE_ASYNC = true;
+		#endif
+	}
+}
+
---
+++ Script/ReThrowException/ReThrowException.cs
@@ -0,0 +1,50 @@
+
+
+/**
+ * Copyright (c) blueback
+ * Released under the MIT License
+ * https://github.com/bluebackblue/fee/blob/master/LICENSE.txt
+ * http://bbbproject.sakura.ne.jp/wordpress/mitlicense
+ * @brief リスロー。
+*/
+
+
+/** Fee.Audio
+*/
+namespace Fee.ReThrowException
+{
+	/** ReThrowException
+	*/
+	[System.Serializable()]
+	class ReThrowException : System.Exception
+	{
+		/** constructor
+		*/
+		public ReThrowException()
+			: base()
+		{
+		}
+
+		/** constructor
+		*/
+		public ReThrowException(string a_message)
+			: base(a_message)
+		{
+		}
+
+		/** constructor
+		*/
+		public ReThrowException(string a_message,System.Exception a_inner_exception)
+			: base(a_message,a_inner_exception)
+		{
+		}
+
+		/** constructor
+		*/
+		protected ReThrowException(System.Runtime.Serialization.SerializationInfo a_info,System.Runtime.Serialization.StreamingContext a_context)
+			: base(a_info,a_context)
+		{
+		}
+	}
+}
+
