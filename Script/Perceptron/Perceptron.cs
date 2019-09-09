cc34e0177f3ba30bb20e7912b75e23632267b3ed
backblue <blueback28@gmail.com>
2019-09-09 09:57:02 +0900

    整理

--- Script/Perceptron/Perceptron.cs
+++ Script/Perceptron/Perceptron.cs
@@ -76,33 +76,19 @@ namespace Fee.Perceptron
 		*/
 		public void ForwardCalculation()
 		{
-			//中間レイヤー。
-			{
-				for(int j = 0;j < this.layer_list[1].node_list.Count;j++){
-					if(this.layer_list[1].node_list[j].is_bias == false){
+			for(int t_layer_index = 1;t_layer_index < this.layer_list.Count;t_layer_index++){
+				//中間レイヤー => 中間レイヤー => 出力レイヤー。
 
+				foreach(Node t_node in this.layer_list[t_layer_index].node_list){
+					if(t_node.is_bias == false){
+						//合計。
 						float t_sum = 0.0f;
-						foreach(Link t_link in this.layer_list[1].node_list[j].link_list_prev){
+						foreach(Link t_link in t_node.link_list_prev){
 							t_sum += t_link.node_from.value * t_link.weight;
 						}
 
 						//シグモイド。
-						this.layer_list[1].node_list[j].value = 1.0f / (1.0f + UnityEngine.Mathf.Exp(-t_sum));
-					}
-				}
-			}
-
-			//出力レイヤー。
-			{
-				for(int j = 0;j < this.layer_list[2].node_list.Count;j++){
-					if(this.layer_list[2].node_list[j].is_bias == false){
-						float t_sum = 0.0f;
-						foreach(Link t_link in this.layer_list[2].node_list[j].link_list_prev){
-							t_sum += t_link.node_from.value * t_link.weight;
-						}
-
-						//シグモイド。
-						this.layer_list[2].node_list[j].value = 1.0f / (1.0f + UnityEngine.Mathf.Exp(-t_sum));
+						t_node.value = 1.0f / (1.0f + UnityEngine.Mathf.Exp(-t_sum));
 					}
 				}
 			}
@@ -112,51 +98,45 @@ namespace Fee.Perceptron
 		*/
 		public void BackPropagation()
 		{
-			//出力レイヤー。
+			//出力レイヤー。誤差。
 			{
-				Layer t_layer_out = this.layer_list[2];
+				Layer t_layer_out = this.layer_list[this.layer_list.Count - 1];
 				Layer t_layer_teacher = this.layer_teacher;
-
 				for(int t_node_index=0;t_node_index<t_layer_teacher.node_list.Count;t_node_index++){
 					Node t_node_teacher = t_layer_teacher.node_list[t_node_index];
 					Node t_node_out = t_layer_out.node_list[t_node_index];
-
-					//誤差。
-					float t_eee_value = t_node_teacher.value - t_node_out.value;
-
-					//0.5fの時が最大。
-					float t_rate = t_node_out.value * (1.0f - t_node_out.value);
-
-					t_node_out.error = t_eee_value * t_rate;
+					t_node_out.error = (t_node_teacher.value - t_node_out.value) * t_node_out.value * (1.0f - t_node_out.value);
 				}
 			}
 
-			//中間レイヤー。
-			for(int t_node_index = 0;t_node_index < this.layer_list[1].node_list.Count;t_node_index++){
-
-				float t_error = 0.0f;
-				foreach(Link t_link in this.layer_list[1].node_list[t_node_index].link_list){
-					t_error += t_link.node_to.error * t_link.weight;
-				}
-
-				this.layer_list[1].node_list[t_node_index].error = t_error * this.layer_list[1].node_list[t_node_index].value * (1.0f - this.layer_list[1].node_list[t_node_index].value);
-			}
-
-			//中間レイヤー。重み。
+			//中間レイヤー。誤差。
 			{
-				for(int t_node_index = 0;t_node_index < this.layer_list[1].node_list.Count; t_node_index++){
-					foreach(Link t_link in this.layer_list[1].node_list[t_node_index].link_list){
-						float t_dw = 
-						t_link.weight += t_link.weight_change_rate * t_link.node_to.error * t_link.node_from.value;
+				for(int t_layer_index = this.layer_list.Count - 1;t_layer_index >= 1;t_layer_index--){
+					//中間レイヤー(出力レイヤー側) => 中間レイヤー（入力レイヤー側）。
+
+					foreach(Node t_node in this.layer_list[t_layer_index].node_list){
+
+						//誤差合計。
+						float t_error_sum = 0.0f;
+						foreach(Link t_link in t_node.link_list){
+							t_error_sum += t_link.node_to.error * t_link.weight;
+						}
+
+						//誤差。
+						t_node.error = t_node.value * (1 - t_node.value) * t_error_sum;
 					}
 				}
 			}
 
-			//入力レイヤー。重み。
+			//重み。
 			{
-				for(int t_node_index = 0;t_node_index < this.layer_list[0].node_list.Count;t_node_index++){
-					foreach(Link t_link in this.layer_list[0].node_list[t_node_index].link_list){
-						t_link.weight += t_link.weight_change_rate * t_link.node_to.error * t_link.node_from.value;
+				for(int t_layer_index = 0;t_layer_index < (this.layer_list.Count - 1);t_layer_index++){
+					//入力レイヤー => 中間レイヤー => 中間レイヤー。
+
+					foreach(Node t_node in this.layer_list[t_layer_index].node_list){
+						foreach(Link t_link in t_node.link_list){
+							t_link.weight += t_link.weight_change_rate * t_link.node_to.error * t_link.node_from.value;
+						}
 					}
 				}
 			}
